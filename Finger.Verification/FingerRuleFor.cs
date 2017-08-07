d1f14f1fd5efc20501e4cadd4aedc0b54e717776
Stage <1196089730@qq.com>
2017-08-07 17:51:34 +0800

    重新初始化项目

---
+++ Finger.Verification/FingerRuleFor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using System.Linq;
+namespace Finger.Verification
+{
+    public class FingerRuleFor: ActionFilterAttribute
+    {
+       override  public  void OnActionExecuted(ActionExecutedContext context)
+        {
+            StringValues  testdsa;
+            var   test = context.HttpContext.Request.Form.TryGetValue("userName",out testdsa);
+            var test1 = context.ActionDescriptor.Parameters.Where(p => p.BindingInfo.BindingSource.Id.Equals("Query")).Select(p=>p.Name).ToArray();
+                int a, b =1;
+        }
+    }
+}
