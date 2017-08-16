aac394e1b92971caf04742dc3efaf1959fbfb192
rosarugosa <1196089730@qq.com>
2017-08-16 15:45:28 +0800

    finish Attribute

---
+++ Finger.Verification/Finger.Verification/FingerRuleForAttribute.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Finger.Verification
+{
+    public class FingerRuleForAttribute:ActionFilterAttribute
+    {
+        private ActionExecutingContext _context;
+        
+        /// <summary>
+        /// int,float,double minvalue
+        /// </summary>
+        public double MinValue
+        {
+            get=>MinValue;
+            set => MinValue = double.MinValue;
+        }
+
+        /// <summary>
+        /// int,float,double Maxvalue
+        /// </summary>
+        public double MaxValue
+        {
+            get=>MaxValue;
+            set => MaxValue = double.MaxValue;
+        }
+
+        /// <summary>
+        /// string min length
+        /// </summary>
+        public double MinLength
+        {
+            get=>MinLength; 
+            set=>MinLength =double.MinValue;
+        }
+
+        /// <summary>
+        /// string max length
+        /// </summary>
+        public double MaxLength
+        {
+            get=>MaxLength;
+            set { MaxLength = double.MaxValue; }
+        }
+
+        /// <summary>
+        /// the parameter is null
+        /// </summary>
+        public bool IsRequired
+        {
+            get => IsRequired;
+            set => IsRequired = true;
+        }
+
+        public bool IsNull
+        {
+            get => IsNull;
+            set => IsNull = true;
+        }
+
+        public FingerRuleForAttribute(string parameterName,bool?isNull,bool? isRequired,double? minLength,double? maxLength)
+        {
+            //string verification
+            
+        }
+
+        public FingerRuleForAttribute(string parameterName,bool? isRequired, double? minValue, double? maxValue)
+        {
+            
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            this._context = context;
+        }
+    }
+}
